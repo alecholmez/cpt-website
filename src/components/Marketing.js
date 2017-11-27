a2b55c60af0704ece5fee4917f5a67a5deb722a9
alecholmez <alecholmez@me.com>
2017-11-26 21:03:08 -0500

    fixed #1, fixed #2

--- src/components/Marketing.js
+++ src/components/Marketing.js
@@ -5,27 +5,25 @@ import React from 'react';
 
 class Marketing extends React.Component {
   render() {
-    return (
-        <div className="uk-section uk-section-muted">
-            <div className="uk-container">
-                <div className="uk-child-width-expand@s" data-uk-grid>
-                    <div>
-                        <h1 className="uk-text-center">{'Meets'}</h1>
-                        <p>{'We meet every Thursday from 6:30 to 7 P.M. in Lot Z of the University Of Maryland, College Park campus.'}</p>
-                    </div>
-                    <div>
-                        <h1 className="uk-text-center">{'About Us'}</h1>
-                        <p>{'We are the official car club of the University of Maryland. We take pride in our culture, but most importantly, our cars.'}</p>
-                    </div>
-                    <div>
-                        <h1 className="uk-text-center">{'Cruises'}</h1>
-                        <p>{'We cruise to a random destination after every meet. Underground car shows, new resteraunts, and various scenic overlooks are perfect spots for admiring our cars.'}</p>
-                    </div>
-                </div>
-
-            </div>
+    return (<div className="uk-section uk-section-muted">
+      <div className="uk-container">
+        <div className="uk-child-width-expand@s" data-uk-grid="data-uk-grid">
+          <div>
+            <h1 className="uk-text-center">{'Meets'}</h1>
+            <p>{'We meet every Thursday from 6:30 to 7 P.M. in Lot Z of the University Of Maryland, College Park campus.'}</p>
+          </div>
+          <div>
+            <h1 className="uk-text-center">{'About Us'}</h1>
+            <p>{'We are the official car club of the University of Maryland. We take pride in our culture, but most importantly, our cars.'}</p>
+          </div>
+          <div>
+            <h1 className="uk-text-center">{'Cruises'}</h1>
+            <p>{'We cruise to a random destination after every meet. Underground car shows, new restaraunts, and various scenic overlooks are perfect spots for admiring our cars.'}</p>
+          </div>
         </div>
-    );
+
+      </div>
+    </div>);
   }
 }
 
--- src/components/Media.js
+++ src/components/Media.js
@@ -1,18 +1,20 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
+require('video-react/dist/video-react.css')
 
 import React from 'react';
+import {Player} from 'video-react';
 
 class Media extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return (
-        <div className="">
-            <video className="video" loop autoPlay>
-                <source src="https://scontent-iad3-1.cdninstagram.com/vp/de7bc8f983141d186258182804bc7813/5A1D8370/t50.2886-16/21684934_2011120419120778_8495749287798374400_n.mp4" type="video/mp4" />
-                Your Browser does not support this video tag.
-            </video>
-        </div>
-    );
+    return (<div className="video">
+      <Player loop="loop" autoPlay="autoPlay" src="https://scontent-iad3-1.cdninstagram.com/vp/de7bc8f983141d186258182804bc7813/5A1D8370/t50.2886-16/21684934_2011120419120778_8495749287798374400_n.mp4"/>
+    </div>);
   }
 }
 

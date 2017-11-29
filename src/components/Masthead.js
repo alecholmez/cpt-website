17a7860ad2707e9db922990e74f76c0f1d3cf13e
Alec Holmes <alecholmez@me.com>
2017-11-28 22:36:52 -0500

    removed video. switched to parallax background

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -6,14 +6,15 @@ import React from 'react';
 class Masthead extends React.Component {
   render() {
     return (
-      <div className="uk-section uk-section-xlarge  uk-section-transparent uk-light uk-text-center">
-        <h1 className="uk-heading-hero">College Park Tuning</h1>
+      <div className="uk-section uk-section-xlarge uk-light uk-text-center uk-background-fixed uk-background-norepeat uk-background-cover masthead-background" data-uk-parallax="bgy: -200">
+          <div className="">
+              <h1 className="uk-heading-hero">College Park Tuning</h1>
+          </div>
       </div>
     );
   }
 }
 
-Masthead.defaultProps = {
-};
+Masthead.defaultProps = {};
 
 export default Masthead;
--- src/components/Media.js
+++ src/components/Media.js
@@ -3,7 +3,6 @@ require('styles/App.css');
 require('video-react/dist/video-react.css')
 
 import React from 'react';
-import {Player} from 'video-react';
 
 class Media extends React.Component {
 
@@ -12,9 +11,11 @@ class Media extends React.Component {
   }
 
   render() {
-    return (<div className="video">
-      <Player loop={true} autoPlay={true} playsInline={true} fluid={true} src="https://scontent-iad3-1.cdninstagram.com/vp/de7bc8f983141d186258182804bc7813/5A1D8370/t50.2886-16/21684934_2011120419120778_8495749287798374400_n.mp4"></Player>
-    </div>);
+    return (
+      <div>
+                
+        </div>
+    );
   }
 }
 
